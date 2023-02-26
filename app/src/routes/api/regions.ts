22234819926535ac9bff6889957d616cc47dff83
wilsonsantos93 <wilsonjesus93@gmail.com>
2023-02-26 18:51:32 +0000

    Added route to get weather for a region id

--- app/src/routes/api/regions.ts
+++ app/src/routes/api/regions.ts
@@ -4,7 +4,7 @@ import express from "express";
 //! Get region borders data route
 //! Client sends a geoJSON to be saved to the database
 //! Calculate centers of each feature in the database route
-import { handleGetRegionBorders } from "../../controllers/api/regions.js";
+import { handleGetRegionBorders, handleGetRegionBorderWithWeather } from "../../controllers/api/regions.js";
 import { authenticateAPI } from "../../utils/routes.js";
 
 const router = express.Router();
@@ -17,5 +17,8 @@ router.get("/:id", authenticateAPI(), async function (request, response) {
   await handleGetRegionBorders(request, response);
 });
 
+router.get("/:id/weather", authenticateAPI(), async function (request, response) {
+  await handleGetRegionBorderWithWeather(request, response);
+});
 
 export default router;
